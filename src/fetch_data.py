28290468eb93d917471e68560c5158086bb96bca
pygodhj <hujin5575362@outlook.com>
2025-12-31 17:23:21 +0800

    完善fetch_data,py

--- src/fetch_data.py
+++ src/fetch_data.py
@@ -2,8 +2,33 @@ import os
 import tkinter as tk
 from tkinter import filedialog
 from typing import Optional
-import tushare as ts
 import pandas as pd
-from database import Sql
 
 
+#类名含义为获取的数据类，实例化对象为每个新获取的数据
+class FetchedData:
+    #从文件中获取数据的方法
+    @classmethod
+    def fetch_file(cls)->pd.DataFrame():
+        path=cls.__file_selector()
+        pass
+
+    #从数据库中获取数据的方法
+    @classmethod
+    def fetch_database(cls)->pd.DataFrame():
+        pass
+
+    #从网络中获取数据的方法
+    @classmethod
+    def fetch_internet(cls)->pd.DataFrame():
+        pass
+
+    #导出数据到文件或数据库的方法
+    @classmethod
+    def export_data(cls,df:pd.DataFrame(),ext):
+        pass
+
+    #调用选取文件的窗口，并选取文件导出文件路径的方法（内部私有函数）
+    @classmethod
+    def __file_selector(cls)->os.path:
+        pass
